2fd571c0da2bc9b3f10e470d87428e00bbaa092b
tomc04 <chen.thomas04@gmail.com>
2024-05-24 16:28:01 -0700

    Semi-Working Camera

--- lib/src/views/camera_screen.dart
+++ lib/src/views/camera_screen.dart
@@ -2,23 +2,11 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
-
-// late List<CameraDescription> _cameras;
-
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   _cameras = await availableCameras();
-//   runApp(CameraScreen(cameras: _cameras));
-//   // runApp(const CameraScreen());
-// }
+import 'package:permission_handler/permission_handler.dart';
 
 /// CameraApp is the Main Application.
 class CameraScreen extends StatefulWidget {
   /// Default Constructor
-  // final List<CameraDescription> cameras;
-  // const CameraScreen({super.key, required this.cameras});
-
   const CameraScreen({super.key});
 
   @override
@@ -29,8 +17,9 @@ class _CameraScreenState extends State<CameraScreen> {
   late CameraController controller;
 
   late Future<void> _initializeControllerFuture;
-
-  //get _cameras async => await availableCameras();
+  
+  bool _isPermissionGrantedCamera = false;
+  bool _isPermissionGrantedMicrophone = false;
 
   @override
   void initState() {
@@ -39,52 +28,43 @@ class _CameraScreenState extends State<CameraScreen> {
   }
 
   Future<void> _initialzeCamera() async {
-    final cameras = await availableCameras();
-    if (cameras.isNotEmpty) {
-      controller = CameraController(cameras[0], ResolutionPreset.max);
-      _initializeControllerFuture = controller.initialize();
-      controller.initialize().then((_) {
-        if (!mounted) {
-          return;
-        }
-        setState(() {});
-      }).catchError((Object e) {
-        if (e is CameraException) {
-          switch (e.code) {
-            case 'CameraAccessDenied':
-              // Handle access errors here.
-              break;
-            default:
-              // Handle other errors here.
-              break;
+    final statusCam = await Permission.camera.request();
+    final statusMic = await Permission.microphone.request();
+    if (statusCam.isGranted && statusMic.isGranted) {
+      setState(() {
+        _isPermissionGrantedCamera = true;
+        _isPermissionGrantedMicrophone = true;
+      });
+      final cameras = await availableCameras();
+      if (cameras.isNotEmpty) {
+        controller = CameraController(cameras[0], ResolutionPreset.max);
+        _initializeControllerFuture = controller.initialize();
+        controller.initialize().then((_) {
+          if (!mounted) {
+            return;
           }
-        }
+          setState(() {});
+        }).catchError((Object e) {
+          if (e is CameraException) {
+            switch (e.code) {
+              case 'CameraAccessDenied':
+                // Handle access errors here.
+                break;
+              default:
+                // Handle other errors here.
+                break;
+            }
+          }
+        });
+      }
+    } else {
+      setState(() {
+        _isPermissionGrantedCamera = false;
+        _isPermissionGrantedMicrophone = false;
       });
     }
-  }
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   controller = CameraController(_cameras[0], ResolutionPreset.max);
-  // controller.initialize().then((_) {
-  //   if (!mounted) {
-  //     return;
-  //   }
-  //   setState(() {});
-  // }).catchError((Object e) {
-  //   if (e is CameraException) {
-  //     switch (e.code) {
-  //       case 'CameraAccessDenied':
-  //         // Handle access errors here.
-  //         break;
-  //       default:
-  //         // Handle other errors here.
-  //         break;
-  //     }
-  //   }
-  // });
-  // }
+  }
 
   @override
   void dispose() {
@@ -93,11 +73,12 @@ class _CameraScreenState extends State<CameraScreen> {
   }
 
   Future<void> _takePicture() async {
-    final imageLocation = await getTemporaryDirectory();
+    final imageLocation = await getTemporaryDirectory(); // possible error
     final imagePath = join(imageLocation.path, '${DateTime.now()}.png');
+    print(imagePath);
     XFile picture = await controller.takePicture();
     picture.saveTo(imagePath);
-    print(imagePath);
+
   }
 
   @override
@@ -113,62 +94,29 @@ class _CameraScreenState extends State<CameraScreen> {
           ]),
           backgroundColor: Colors.white,
           elevation: 0.0),
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (!controller.value.isInitialized) {
-              return Container();
-            }
-            return Center(
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * 0.95,
-                width: MediaQuery.of(context).size.width * 0.95,
-                child: CameraPreview(controller),
-              ),
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
+      body: (_isPermissionGrantedCamera && _isPermissionGrantedMicrophone)
+          ? FutureBuilder<void>(
+              future: _initializeControllerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (!controller.value.isInitialized) {
+                    return Container();
+                  }
+                  return Center(
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.95,
+                      width: MediaQuery.of(context).size.width * 0.95,
+                      child: CameraPreview(controller),
+                    ),
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            )
+          : const Center(
+              child: Text('Waiting for camera and microphone permissions...',
+                  style: TextStyle(fontSize: 16))),
     );
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     backgroundColor: Colors.white,
-  //     appBar: AppBar(
-  //       title: const Row(
-  //         children: [
-  //         Icon(Icons.food_bank, color: Colors.green, size: 40),
-  //         SizedBox(width: 20),
-  //         Text("Food Focus", style: TextStyle(color: Colors.green)), ]
-  //       ),
-  //       backgroundColor: Colors.white,
-  //       elevation: 0.0
-  //     ),
-  //     body: FutureBuilder<void>(
-  //       future: _initializeControllerFuture,
-  //       builder: (context, snapshot) {
-  //         if (snapshot.connectionState == ConnectionState.done) {
-  //           if (!controller.value.isInitialized) {
-  //             return Container();
-  //           }
-  //           return Center(
-  //             child: SizedBox(
-  //               height: MediaQuery.of(context).size.height * 0.95,
-  //               width: MediaQuery.of(context).size.width * 0.95,
-  //               child: CameraPreview(controller),
-  //             ),
-  //           );
-  //         } else {
-  //           return const Center(child: CircularProgressIndicator());
-  //         }
-  //       },
-  //     ),
-
-  //   );
-  // }
 }
